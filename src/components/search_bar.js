ad90f5bbf3ed80c1b9f35a663c974d8610cd7de3
Cole-Campbell <cole.william.campbell@gmail.com>
2017-08-02 14:27:53 +0100

    Added State to the class constructor of the Search Bar to allow for the value of the input to be rerendered upon changes by the user, and to reflect those changes in the DOM.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 //Exporting Modules, Classes, State
@@ -11,10 +11,35 @@ const SearchBar = () => {
 }
 */
 //Class component. Internal Record Keeping. Aware of itself and others.
-//Declaration of a new class. JavaScript Object.
-class SearchBar extends React.Component{
+//Declaration of a new class. JavaScript Object. Removed React.Component as Component has been added to the import.
 
+//Use of arrow function to condense code.
+class SearchBar extends Component{
+  //State is a plain JS object used to record and react to user events.
+  //Only class based components can have state.
+  constructor(props) {
+    super(props);
+
+    //ID Can be set to anything
+    this.state = { term: '' };
+  }
+
+  render() {
+    //Manipulating State by setState
+    return (
+      <div>
+        <input onChange = {e => this.setState({ term: e.target.value })} />
+        Value of the input: {this.state.term}
+      </div>
+    )
+  }
+
+  /*Method for change. Method condensed into class with Arrow Function
+  onInputChange(e) {
+    console.log(e);
+  }
 }
-
+*/
+}
 
 export default SearchBar;
