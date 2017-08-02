4d5fe52700d5c390c53f5785b6002ff4602fc12c
Cole-Campbell <cole.william.campbell@gmail.com>
2017-08-02 16:40:40 +0100

    Added array of video objects pulled from Youtube API. The data from the API data is being placed into elements to be represented on the DOM. Can play initial video, but working on video selection.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 //Need to give file reference for files
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 import YTKey from './services/youtube-api';
-
-YTSearch({key: YTKey, term: 'Cats'}, function (data) {
-  console.log(data);
-});
+import VideoDetail from './components/video_detail'
 
 // Create a new component. This should produce HTML
 //ES6 Syntax
-const App = () => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: YTKey, term: 'Cats'}, (videos) => {
+      this.setState({ videos });
+      console.log(videos);
+    });
+  }
+  render() {
   return (
     <div>
       <SearchBar />
+      <VideoDetail video={this.state.videos[0]} />
+      <VideoList videos={this.state.videos} />
     </div>
-  );
+  )};
 }
+//Data is being passed through the use of PROPS
 
 // Render the component to the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
